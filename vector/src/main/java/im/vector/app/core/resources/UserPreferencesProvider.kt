vCard/element-android

Allow using the latest user avatar and display name for all messages in the timeline
Signed-off-by: Jorge Martín Espinosa <jorgem@element.io>

vector/src/main/java/im/vector/app/core/resources/UserPreferencesProvider.kt
@@ -52,4 +52,8 @@ class UserPreferencesProvider @Inject constructor(private val vectorPreferences:
     fun areThreadMessagesEnabled(): Boolean {
         return vectorPreferences.areThreadMessagesEnabled()
     }
+
+    fun useLiveSenderInfo(): Boolean {
+        return vectorPreferences.useLiveSenderInfo()
+    }
 }

vector/src/main/java/im/vector/app/features/home/room/detail/timeline/helper/TimelineSettingsFactory.kt
@@ -26,7 +26,8 @@ class TimelineSettingsFactory @Inject constructor(private val userPreferencesPro
         return TimelineSettings(
                 initialSize = 30,
                 buildReadReceipts = userPreferencesProvider.shouldShowReadReceipts(),
-                rootThreadEventId = rootThreadEventId
+                rootThreadEventId = rootThreadEventId,
+                useLiveSenderInfo = userPreferencesProvider.useLiveSenderInfo()
         )
     }
 }

VoyageClient/Voyage

vector/src/main/java/im/vector/app/core/resources/UserPreferencesProvider.kt
@@ -52,4 +52,8 @@ class UserPreferencesProvider @Inject constructor(private val vectorPreferences:
     fun areThreadMessagesEnabled(): Boolean {
         return vectorPreferences.areThreadMessagesEnabled()
     }
+
+    fun showLiveSenderInfo(): Boolean {
+        return vectorPreferences.showLiveSenderInfo()
+    }
 }

vector/src/main/java/im/vector/app/features/home/room/detail/timeline/helper/TimelineSettingsFactory.kt
@@ -26,7 +26,8 @@ class TimelineSettingsFactory @Inject constructor(private val userPreferencesPro
         return TimelineSettings(
                 initialSize = 30,
                 buildReadReceipts = userPreferencesProvider.shouldShowReadReceipts(),
-                rootThreadEventId = rootThreadEventId
+                rootThreadEventId = rootThreadEventId,
+                useLiveSenderInfo = userPreferencesProvider.showLiveSenderInfo()
         )
     }
 }