element-hq/element-android

extracting registration action business logic to the handler abstraction and adding tests - renames the existing handler to a wizard delegate

vector/src/main/java/im/vector/app/features/onboarding/RegistrationActionHandler.kt
@@ -16,52 +16,88 @@
 
 package im.vector.app.features.onboarding
 
-import org.matrix.android.sdk.api.auth.registration.RegisterThreePid
+import im.vector.app.R
+import im.vector.app.core.resources.StringProvider
+import im.vector.app.core.utils.ensureTrailingSlash
+import im.vector.app.features.VectorFeatures
+import im.vector.app.features.VectorOverrides
+import im.vector.app.features.login.isSupported
+import im.vector.app.features.onboarding.ftueauth.MatrixOrgRegistrationStagesComparator
+import kotlinx.coroutines.flow.first
+import org.matrix.android.sdk.api.auth.AuthenticationService
+import org.matrix.android.sdk.api.auth.registration.FlowResult
 import org.matrix.android.sdk.api.auth.registration.RegistrationResult
-import org.matrix.android.sdk.api.auth.registration.RegistrationWizard
+import org.matrix.android.sdk.api.auth.registration.Stage
+import org.matrix.android.sdk.api.session.Session
 import javax.inject.Inject
 
-class RegistrationActionHandler @Inject constructor() {
+class RegistrationActionHandler @Inject constructor(
+        private val registrationWizardActionDelegate: RegistrationWizardActionDelegate,
+        private val authenticationService: AuthenticationService,
+        private val vectorOverrides: VectorOverrides,
+        private val vectorFeatures: VectorFeatures,
+        stringProvider: StringProvider
+) {
 
-    suspend fun handleRegisterAction(registrationWizard: RegistrationWizard, action: RegisterAction): RegistrationResult {
-        return when (action) {
-            RegisterAction.StartRegistration               -> registrationWizard.getRegistrationFlow()
-            is RegisterAction.CaptchaDone                  -> registrationWizard.performReCaptcha(action.captchaResponse)
-            is RegisterAction.AcceptTerms                  -> registrationWizard.acceptTerms()
-            is RegisterAction.RegisterDummy                -> registrationWizard.dummy()
-            is RegisterAction.AddThreePid                  -> registrationWizard.addThreePid(action.threePid)
-            is RegisterAction.SendAgainThreePid            -> registrationWizard.sendAgainThreePid()
-            is RegisterAction.ValidateThreePid             -> registrationWizard.handleValidateThreePid(action.code)
-            is RegisterAction.CheckIfEmailHasBeenValidated -> registrationWizard.checkIfEmailHasBeenValidated(action.delayMillis)
-            is RegisterAction.CreateAccount                -> registrationWizard.createAccount(action.username, action.password, action.initialDeviceName)
+    private val matrixOrgUrl = stringProvider.getString(R.string.matrix_org_server_url).ensureTrailingSlash()
+
+    suspend fun processAction(state: SelectedHomeserverState, action: RegisterAction): Result {
+        val result = registrationWizardActionDelegate.executeAction(action)
+        return when {
+            action.ignoresResult() -> Result.Ignored
+            else                   -> when (result) {
+                is RegistrationResult.Success      -> Result.Success(result.session)
+                is RegistrationResult.FlowResponse -> processFlowResult(result, state)
+            }
         }
+    }
+
+    private suspend fun processFlowResult(result: RegistrationResult.FlowResponse, state: SelectedHomeserverState): Result {
+        // If dummy stage is mandatory, and password is already sent, do the dummy stage now
+        return if (authenticationService.isRegistrationStarted && result.flowResult.missingStages.hasMandatoryDummy()) {
+            processAction(state, RegisterAction.RegisterDummy)
+        } else {
+            handleNextStep(state, result.flowResult)
+        }
+    }
+
+    private suspend fun handleNextStep(state: SelectedHomeserverState, flowResult: FlowResult): Result {
+        return when {
+            flowResult.registrationShouldFallback()     -> Result.UnsupportedStage
+            authenticationService.isRegistrationStarted -> findNextStage(state, flowResult)
+            else                                        -> Result.StartRegistration
+        }
+    }
+
+    private fun findNextStage(state: SelectedHomeserverState, flowResult: FlowResult): Result {
+        val orderedResult = when {
+            state.hasSelectedMatrixOrg() && vectorFeatures.isOnboardingCombinedRegisterEnabled() -> flowResult.copy(
+                    missingStages = flowResult.missingStages.sortedWith(MatrixOrgRegistrationStagesComparator())
+            )
+            else                                                                                 -> flowResult
+        }
+        return orderedResult.findNextRegistrationStage()
+                ?.let { Result.NextStage(it) }
+                ?: Result.MissingNextStage
+    }
+
+    private fun FlowResult.findNextRegistrationStage() = missingStages.firstMandatoryOrNull() ?: missingStages.ignoreDummy().firstOptionalOrNull()
+
+    private suspend fun FlowResult.registrationShouldFallback() = vectorOverrides.forceLoginFallback.first() || missingStages.any { !it.isSupported() }
+
+    private fun SelectedHomeserverState.hasSelectedMatrixOrg() = userFacingUrl == matrixOrgUrl
+
+    sealed interface Result {
+        data class Success(val session: Session) : Result
+        data class NextStage(val stage: Stage) : Result
+        object MissingNextStage : Result
+        object StartRegistration : Result
+        object UnsupportedStage : Result
+        object Ignored : Result
     }
 }
 
-sealed interface RegisterAction {
-    object StartRegistration : RegisterAction
-    data class CreateAccount(val username: String, val password: String, val initialDeviceName: String) : RegisterAction
-
-    data class AddThreePid(val threePid: RegisterThreePid) : RegisterAction
-    object SendAgainThreePid : RegisterAction
-
-    // TODO Confirm Email (from link in the email, open in the phone, intercepted by the app)
-    data class ValidateThreePid(val code: String) : RegisterAction
-
-    data class CheckIfEmailHasBeenValidated(val delayMillis: Long) : RegisterAction
-
-    data class CaptchaDone(val captchaResponse: String) : RegisterAction
-    object AcceptTerms : RegisterAction
-    object RegisterDummy : RegisterAction
-}
-
-fun RegisterAction.ignoresResult() = when (this) {
-    is RegisterAction.AddThreePid       -> true
-    is RegisterAction.SendAgainThreePid -> true
-    else                                -> false
-}
-
-fun RegisterAction.hasLoadingState() = when (this) {
-    is RegisterAction.CheckIfEmailHasBeenValidated -> false
-    else                                           -> true
-}
+private fun List<Stage>.firstMandatoryOrNull() = firstOrNull { it.mandatory }
+private fun List<Stage>.firstOptionalOrNull() = firstOrNull { !it.mandatory }
+private fun List<Stage>.ignoreDummy() = filter { it !is Stage.Dummy }
+private fun List<Stage>.hasMandatoryDummy() = any { it is Stage.Dummy && it.mandatory }

VoyageClient/Voyage

vector/src/main/java/im/vector/app/features/onboarding/RegistrationActionHandler.kt
@@ -16,105 +16,91 @@
 
 package im.vector.app.features.onboarding
 
+import im.vector.app.R
+import im.vector.app.core.resources.StringProvider
+import im.vector.app.core.utils.ensureTrailingSlash
+import im.vector.app.features.VectorFeatures
+import im.vector.app.features.VectorOverrides
+import im.vector.app.features.login.isSupported
+import im.vector.app.features.onboarding.ftueauth.MatrixOrgRegistrationStagesComparator
+import kotlinx.coroutines.flow.first
+import org.matrix.android.sdk.api.auth.AuthenticationService
 import org.matrix.android.sdk.api.auth.registration.FlowResult
-import org.matrix.android.sdk.api.auth.registration.RegisterThreePid
-import org.matrix.android.sdk.api.auth.registration.RegistrationResult.FlowResponse
-import org.matrix.android.sdk.api.auth.registration.RegistrationResult.Success
-import org.matrix.android.sdk.api.auth.registration.RegistrationWizard
-import org.matrix.android.sdk.api.failure.is401
+import org.matrix.android.sdk.api.auth.registration.Stage
 import org.matrix.android.sdk.api.session.Session
 import javax.inject.Inject
-import org.matrix.android.sdk.api.auth.registration.RegistrationResult as MatrixRegistrationResult
 
-class RegistrationActionHandler @Inject constructor() {
+class RegistrationActionHandler @Inject constructor(
+        private val registrationWizardActionDelegate: RegistrationWizardActionDelegate,
+        private val authenticationService: AuthenticationService,
+        private val vectorOverrides: VectorOverrides,
+        private val vectorFeatures: VectorFeatures,
+        stringProvider: StringProvider
+) {
 
-    suspend fun handleRegisterAction(registrationWizard: RegistrationWizard, action: RegisterAction): RegistrationResult {
-        return when (action) {
-            RegisterAction.StartRegistration -> resultOf { registrationWizard.getRegistrationFlow() }
-            is RegisterAction.CaptchaDone -> resultOf { registrationWizard.performReCaptcha(action.captchaResponse) }
-            is RegisterAction.AcceptTerms -> resultOf { registrationWizard.acceptTerms() }
-            is RegisterAction.RegisterDummy -> resultOf { registrationWizard.dummy() }
-            is RegisterAction.AddThreePid -> handleAddThreePid(registrationWizard, action)
-            is RegisterAction.SendAgainThreePid -> resultOf { registrationWizard.sendAgainThreePid() }
-            is RegisterAction.ValidateThreePid -> resultOf { registrationWizard.handleValidateThreePid(action.code) }
-            is RegisterAction.CheckIfEmailHasBeenValidated -> handleCheckIfEmailIsValidated(registrationWizard, action.delayMillis)
-            is RegisterAction.CreateAccount -> resultOf {
-                registrationWizard.createAccount(
-                        action.username,
-                        action.password,
-                        action.initialDeviceName
-                )
+    private val matrixOrgUrl = stringProvider.getString(R.string.matrix_org_server_url).ensureTrailingSlash()
+
+    suspend fun processAction(state: SelectedHomeserverState, action: RegisterAction): Result {
+        val result = registrationWizardActionDelegate.executeAction(action)
+        return when {
+            action.ignoresResult() -> Result.Ignored
+            else                   -> when (result) {
+                is RegistrationResult.Complete         -> Result.Success(result.session)
+                is RegistrationResult.NextStep         -> processFlowResult(result, state)
+                is RegistrationResult.SendEmailSuccess -> Result.SendEmailSuccess(result.email)
+                is RegistrationResult.Error            -> Result.Error(result.cause)
             }
         }
     }
 
-    private suspend fun handleAddThreePid(wizard: RegistrationWizard, action: RegisterAction.AddThreePid): RegistrationResult {
-        return runCatching { wizard.addThreePid(action.threePid) }.fold(
-                onSuccess = { it.toRegistrationResult() },
-                onFailure = {
-                    when {
-                        action.threePid is RegisterThreePid.Email && it.is401() -> RegistrationResult.SendEmailSuccess(action.threePid.email)
-                        else -> RegistrationResult.Error(it)
-                    }
-                }
-        )
+    private suspend fun processFlowResult(result: RegistrationResult.NextStep, state: SelectedHomeserverState): Result {
+        // If dummy stage is mandatory, and password is already sent, do the dummy stage now
+        return if (authenticationService.isRegistrationStarted() && result.flowResult.missingStages.hasMandatoryDummy()) {
+            processAction(state, RegisterAction.RegisterDummy)
+        } else {
+            handleNextStep(state, result.flowResult)
+        }
     }
 
-    private tailrec suspend fun handleCheckIfEmailIsValidated(registrationWizard: RegistrationWizard, delayMillis: Long): RegistrationResult {
-        return runCatching { registrationWizard.checkIfEmailHasBeenValidated(delayMillis) }.fold(
-                onSuccess = { it.toRegistrationResult() },
-                onFailure = {
-                    when {
-                        it.is401() -> null // recursively continue to check with a delay
-                        else -> RegistrationResult.Error(it)
-                    }
-                }
-        ) ?: handleCheckIfEmailIsValidated(registrationWizard, 10_000)
+    private suspend fun handleNextStep(state: SelectedHomeserverState, flowResult: FlowResult): Result {
+        return when {
+            flowResult.registrationShouldFallback()       -> Result.UnsupportedStage
+            authenticationService.isRegistrationStarted() -> findNextStage(state, flowResult)
+            else                                          -> Result.StartRegistration
+        }
+    }
+
+    private fun findNextStage(state: SelectedHomeserverState, flowResult: FlowResult): Result {
+        val orderedResult = when {
+            state.hasSelectedMatrixOrg() && vectorFeatures.isOnboardingCombinedRegisterEnabled() -> flowResult.copy(
+                    missingStages = flowResult.missingStages.sortedWith(MatrixOrgRegistrationStagesComparator())
+            )
+            else                                                                                 -> flowResult
+        }
+        return orderedResult.findNextRegistrationStage()
+                ?.let { Result.NextStage(it) }
+                ?: Result.MissingNextStage
+    }
+
+    private fun FlowResult.findNextRegistrationStage() = missingStages.firstMandatoryOrNull() ?: missingStages.ignoreDummy().firstOptionalOrNull()
+
+    private suspend fun FlowResult.registrationShouldFallback() = vectorOverrides.forceLoginFallback.first() || missingStages.any { !it.isSupported() }
+
+    private fun SelectedHomeserverState.hasSelectedMatrixOrg() = userFacingUrl == matrixOrgUrl
+
+    sealed interface Result {
+        data class Success(val session: Session) : Result
+        data class NextStage(val stage: Stage) : Result
+        data class Error(val cause: Throwable) : Result
+        data class SendEmailSuccess(val email: String) : Result
+        object MissingNextStage : Result
+        object StartRegistration : Result
+        object UnsupportedStage : Result
+        object Ignored : Result
     }
 }
 
-private inline fun resultOf(block: () -> MatrixRegistrationResult): RegistrationResult {
-    return runCatching { block() }.fold(
-            onSuccess = { it.toRegistrationResult() },
-            onFailure = { RegistrationResult.Error(it) }
-    )
-}
-
-private fun MatrixRegistrationResult.toRegistrationResult() = when (this) {
-    is FlowResponse -> RegistrationResult.NextStep(flowResult)
-    is Success -> RegistrationResult.Complete(session)
-}
-
-sealed interface RegistrationResult {
-    data class Error(val cause: Throwable) : RegistrationResult
-    data class Complete(val session: Session) : RegistrationResult
-    data class NextStep(val flowResult: FlowResult) : RegistrationResult
-    data class SendEmailSuccess(val email: String) : RegistrationResult
-}
-
-sealed interface RegisterAction {
-    object StartRegistration : RegisterAction
-    data class CreateAccount(val username: String, val password: String, val initialDeviceName: String) : RegisterAction
-
-    data class AddThreePid(val threePid: RegisterThreePid) : RegisterAction
-    object SendAgainThreePid : RegisterAction
-
-    // TODO Confirm Email (from link in the email, open in the phone, intercepted by the app)
-    data class ValidateThreePid(val code: String) : RegisterAction
-
-    data class CheckIfEmailHasBeenValidated(val delayMillis: Long) : RegisterAction
-
-    data class CaptchaDone(val captchaResponse: String) : RegisterAction
-    object AcceptTerms : RegisterAction
-    object RegisterDummy : RegisterAction
-}
-
-fun RegisterAction.ignoresResult() = when (this) {
-    is RegisterAction.SendAgainThreePid -> true
-    else -> false
-}
-
-fun RegisterAction.hasLoadingState() = when (this) {
-    is RegisterAction.CheckIfEmailHasBeenValidated -> false
-    else -> true
-}
+private fun List<Stage>.firstMandatoryOrNull() = firstOrNull { it.mandatory }
+private fun List<Stage>.firstOptionalOrNull() = firstOrNull { !it.mandatory }
+private fun List<Stage>.ignoreDummy() = filter { it !is Stage.Dummy }
+private fun List<Stage>.hasMandatoryDummy() = any { it is Stage.Dummy && it.mandatory }